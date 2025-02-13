4fdb1f87bcd6cf15d77fbf4c19d03bc0641a3d63
Miguel Wiest <wiestmiguel@gmail.com>
2025-02-12 23:27:59 -0300

    feat

--- src/shared/components/nav/index.tsx
+++ src/shared/components/nav/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+"use client"; // Adicione esta linha no topo do arquivo
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 import "./styles.scss";
 
 export const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const paths = [
     {
       title: "Quem somos",
@@ -31,12 +34,29 @@ export const Nav = () => {
       href: "/contato",
     },
   ];
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="nav__container">
       <Link href="/">
         <Image src="/logo.png" alt="Logo" width={100} height={100} />
       </Link>
-      <ul className="nav__links">
+
+      {/* Botão do hamburger */}
+      <button className="nav__hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      {/* Lista de links */}
+      <ul className={`nav__links ${isMenuOpen ? "nav__links--open" : ""}`}>
+        <span onClick={() => {
+          setIsMenuOpen(false);
+        }}>x</span>
         {paths.map((path) => (
           <li key={path.href} className="nav__link">
             <Link href={path.href}>{path.title}</Link>
